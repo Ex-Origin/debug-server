2710c91cd2fea870499f4d6d34a5e22e42c0726b
Cong Li <2462148389@qq.com>
2023-05-30 08:29:48 +0800

    Update attach.py

--- attach.py
+++ attach.py
@@ -7,19 +7,31 @@ import os
 
 attach_host = '172.17.0.2'
 attach_port = 9545
-def attach():
+def attach(script=''):
     tmp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    gdbinit = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'gdbscript.sh').encode()
+    gdb_script = '''
+define pr
+    x/16gx $rebase(0x0)
+end
+
+b *$rebase(0x0)
+    ''' + '\n' + script
+    gdbinit = '/tmp/gdb_script_' + attach_host
+    script_f = open(gdbinit, 'w')
+    script_f.write(gdb_script)
+    script_f.close()
     if attach_host == '': raise ValueError("Please must configure attach_host")
-    tmp_sock.sendto(struct.pack('BB', 0x02, len(gdbinit)) + gdbinit, (attach_host, attach_port))
+    tmp_sock.sendto(struct.pack('BB', 0x02, len(gdbinit.encode())) + gdbinit.encode(), (attach_host, attach_port))
     tmp_sock.recvfrom(4096)
     tmp_sock.close()
+    print('attach successfully')
 def strace():
     tmp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # UDP
     if attach_host == '': raise ValueError("Please must configure attach_host")
     tmp_sock.sendto(struct.pack('B', 0x03), (attach_host, attach_port))
     tmp_sock.recvfrom(4096)
     tmp_sock.close()
+    print('strace successfully')
 def address(search:str)->int:
     tmp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     if attach_host == '': raise ValueError("Please must configure attach_host")
