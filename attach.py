cf0ab08381c2c51ada677f51d05ffb8ae7f6e5f5
Cong Li <2462148389@qq.com>
2023-05-30 08:38:03 +0800

    Ignore the gdb_script in attach.py

--- attach.py
+++ attach.py
@@ -9,17 +9,7 @@ attach_host = '172.17.0.2'
 attach_port = 9545
 def attach(script=''):
     tmp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    gdb_script = '''
-define pr
-    x/16gx $rebase(0x0)
-end
-
-b *$rebase(0x0)
-    ''' + '\n' + script
     gdbinit = '/tmp/gdb_script_' + attach_host
-    script_f = open(gdbinit, 'w')
-    script_f.write(gdb_script)
-    script_f.close()
     if attach_host == '': raise ValueError("Please must configure attach_host")
     tmp_sock.sendto(struct.pack('BB', 0x02, len(gdbinit.encode())) + gdbinit.encode(), (attach_host, attach_port))
     tmp_sock.recvfrom(4096)
