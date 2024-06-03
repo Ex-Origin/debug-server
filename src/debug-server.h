dd5a2c1a842462ac4dcf74157e3e005193f46c54
Cong Li <2462148389@qq.com>
2024-06-03 21:44:12 +0800

    Switch to version 1.4.0

--- src/debug-server.h
+++ src/debug-server.h
@@ -5,7 +5,7 @@
 #define COMMAND_PORT    9545
 #define GDBSERVER_PORT  9549
 
-#define VERSION         "1.3.3"
+#define VERSION         "1.4.0"
 
 #define COMMAND_GDB_REGISTER        0x01
 #define COMMAND_GDBSERVER_ATTACH    0x02
